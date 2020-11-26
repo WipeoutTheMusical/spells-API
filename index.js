dbe25b4b22a486517679bb351e454e871c2e6a49
WipeoutTheMusical <71195464+WipeoutTheMusical@users.noreply.github.com>
2020-11-25 16:09:18 -0800

    Add files via upload
    Added data to the spells.db, and added a SQL query to the actual Javascript. Added get request to the javascript. Response is the name of the spell.

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 const express = require('express');
 const app = express();
-const sqlite3 = require('sqlite3')
+const sqlite3 = require('sqlite3');
+
+app.listen(3000, () => {
+    console.log('The spellbook is listening at port 3000')
+});
 
 app.use(express.json());
-
-let spell = [
-    { spell_name: 'Fireball' },
-];
-app.listen(3000, () => {
-    console.log('Spellbook listening at port 3000...')
-})
-let con = new sqlite3.Database('./spells.db', sqlite3.OPEN_READWRITE, (err) => {
+//This just opens a connection to the database in question, for reading
+let con = new sqlite3.Database('./spells.db', sqlite3.OPEN_READ, (err) => { 
     if (err) {
         console.error(err.message);
     }
     console.log('Connected to the spellbook.');
 });
-app.get('/', function (req, res) {
-    res.send('Greetings from your friendly spellbook.')
-});
-app.post('/api/spells', (req, res) => {
-    let spell = {
-        id: 1,
-        spell_name: req.body.spell_name
-    };
-    spells.push(spells);
-    res.send(spell);
-});
-app.get('/api/spells/1', (req, res) => {
-    res.send(spell);
+//SQL Query for the name of the spell with given row ID.
+    let sql = 'SELECT spell_name FROM spell_info WHERE spell_id = ?';
+    let spell_id = 1;
+
+con.get (sql, [spell_id], (err, row) => {
+if (err){
+    return console.error(err.message)
+}
+return row
+? app.get ('/api/spells/1', (req, res) => {
+    res.send(row.spell_name)
 })
+: console.log('Error: No spell found with that ID')
+});
 con.close((err) => {
     if (err) {
         return console.error(err.message);
     }
-    console.log('Close the spellbook.');
+    console.log('Closing the spellbook.');
 });
