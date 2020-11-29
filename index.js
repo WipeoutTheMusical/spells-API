ba8c6f426046f45145b4b1fdf53e340d912bdf5c
WipeoutTheMusical <71195464+WipeoutTheMusical@users.noreply.github.com>
2020-11-28 22:42:56 -0800

    Add files via upload
    Updated the code to respond to POST requests at endpoint /api/spells/8, and DELETE requests at the same by adding a spell, and deleting the same spell respectively. Also added a PUT request that fixes a misspelling of one of the database entries. 
    TODO:
    Add a revert function that reverts Black Tenticles to the misspelled state, so that it can be run more than once without updating the database manually.
    Fix various things, including a vulnerability to SQL injection (I think it is, anyway). (Since this isn't production code, I did that long line of SQL that way, but normally, I wouldn't list data in the SQL, and would just list the data as "?", and use an array.)

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const app = express();
 const sqlite3 = require('sqlite3');
 
 app.listen(3000, () => {
-    console.log('The spellbook is listening at port 3000')
+    console.log('The server is listening at port 3000')
 });
 
 app.use(express.json());
-let spellArray = [];
+let yesNo = 'n';
 //This just opens a connection to the database in question, for reading
 let con = new sqlite3.Database('./spells.db', sqlite3.OPEN_READ, (err) => { 
     if (err) {
         console.error(err.message);
     }
-    console.log('Connected to the spellbook.');
+    console.log('Connected to the Spellbook.');
 });
 //SQL Query for the name of the spell with given row ID.
     let sql = 'SELECT spell_name FROM spell_info WHERE spell_id = ?';
@@ -26,36 +26,69 @@ if (err){
 return row
 ? app.get ('/api/spells/1', (req, res) => {
     res.send(row.spell_name)
+    con.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Closing the Spellbook.');
+    });
 })
 
 : console.log('Error: No spell found with that ID')
 });
 
-sql = 'SELECT spell_name FROM spell_info WHERE spell_id = ?';
-spell_id = 2;
-con.get (sql, [spell_id], (err, row) => {
-    if (err){
-        return console.error(err.message)
-    }
-    else {
-        const spell2 = {
-            id: spell_id,
-            name: row ? row.spell_name : "Error cannot find spell with that name"
+app.post('/api/spells/8', (req, res) => {
+    con.run('INSERT INTO spell_info(spell_id, spell_name, spell_range, concentration, is_cantrip, bard, cleric, druid, paladin, ranger, sorcerer, warlock, wizard) VALUES(8, "Tashas Caustic Brew", 30, "yes", "no", "no", "no", "no", "no", "no", "yes", "yes", "no")', (err) => {
+        if(err) {
+            return console.log(err.message);
         }
-        
-        return app.post('/api/spells/2', (req, res) => {
-            // TODO: read spell_name parameter from POST request
-            // TODO: insert new spell_name into db
-            // TODO: construct spell2 object here with new spell_name
-            res.send(spell2)
-        })
-    }
-})
-
-
-con.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Closing the spellbook.');
+        else{
+        res.send('Tashas Caustic Brew added to the Spellbook.')
+            con.close((err) => {
+                if (err) {
+                    return console.error(err.message);
+                }
+                console.log('Closing the Spellbook.');
+            });
+        }
+    })
 });
+
+app.put('/api/spells/5', (req, res) => {
+    let updateSpell = 'UPDATE spell_info SET spell_name = ? WHERE spell_id = ?';
+    let values = ['Black Tenticles', 5];
+    con.run(updateSpell, values, function(err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        else {
+            res.send('Updated Black Tenticles');
+            con.close((err) => {
+                if (err) {
+                    return console.error(err.message);
+                }
+                console.log('Closing the Spellbook.');
+            }); 
+        }
+    })
+});
+//Send a DELETE request to the server, deleting the spell that was added earlier.
+app.delete('/api/spells/8', (req, res) =>{
+    let deleteTashas = 'DELETE FROM spell_info WHERE spell_id = ?';
+    let spell_id = 8;
+    con.run(deleteTashas, spell_id, function(err){
+        if (err){
+            return console.error(err.message);
+        }
+
+        else{
+            res.send('Erased Tashas Caustic Brew from the Spellbook.');
+            con.close((err) => {
+                if (err) {
+                    return console.error(err.message);
+                }
+                console.log('Closing the spellbook.');
+            });
+        }
+    })
+})
